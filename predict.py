23dbec92178488c566003e0caba6133c812d3198
adriankucharski <adrian1998.25@live.com>
2022-04-01 00:49:48 +0200

    Add predict

--- predict.py
+++ predict.py
@@ -1,3 +1,4 @@
+from glob import glob
 from skimage import io, filters, morphology
 from skimage.morphology import square
 import os
@@ -7,7 +8,7 @@ from dataset import load_dataset, DataIterator, HexagonDataIterator
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import Model, load_model
 from util import add_salt_and_pepper, normalization
-from typing import Tuple
+from typing import Tuple, Union
 
 
 def generate_dataset(generator_path: str, num_of_data: int,
@@ -44,40 +45,110 @@ def generate_dataset(generator_path: str, num_of_data: int,
         mask = 1 - mask
     return image, mask
 
+class UnetPrediction():
+    def __init__(self, model_path: str, patch_size: int = 64, stride:int = 4, batch_size: int = 64):
+        self.model: Model = load_model(model_path)
+        self.patch_size = patch_size
+        self.stride = stride
+        self.batch_size = batch_size
+    
+    def _build_img_from_patches(self, preds: np.ndarray, img_h:int, img_w:int) -> np.ndarray:
+        patch_h, patch_w = preds.shape[1:3]
+
+        H = (img_h-patch_h)//self.stride+1
+        W = (img_w-patch_w)//self.stride+1
+        prob = np.zeros((img_h, img_w, 1))
+        _sum = np.zeros((img_h, img_w, 1))
+
+        k = 0
+        for h in range(H):
+            for w in range(W):
+                prob[h*self.stride:(h*self.stride)+patch_h, w *
+                    self.stride:(w*self.stride)+patch_w, :] += preds[k]
+                _sum[h*self.stride:(h*self.stride)+patch_h, w *
+                    self.stride:(w*self.stride)+patch_w, :] += 1
+                k += 1
+        final_avg = prob/_sum
+        return final_avg
+
+    def _get_patches(self, img: np.ndarray) -> np.ndarray:
+        h, w = img.shape[0], img.shape[1]
+
+        H = (h-self.patch_size)//self.stride+1
+        W = (w-self.patch_size)//self.stride+1
+
+        patches = np.empty((W*H, self.patch_size, self.patch_size, 1))
+        iter_tot = 0
+        for h in range(H):
+            for w in range(W):
+                patches[iter_tot] = (img[h*self.stride:(h*self.stride)+self.patch_size,
+                                        w*self.stride:(w*self.stride)+self.patch_size, :])
+                iter_tot += 1
+        return patches
+
+    def _add_outline(self, img: np.ndarray) -> np.ndarray:
+        h, w = img.shape[:2]
+        over_h = (h-self.patch_size) % self.stride
+        over_w = (w-self.patch_size) % self.stride
+
+        if (over_h != 0):
+            tmp = np.zeros((h+(self.stride-over_h), w, 1))
+            tmp[0:h, 0:w, :] = img
+            img = tmp
+        if (over_w != 0):
+            tmp = np.zeros((img.shape[0], w+(self.stride - over_w), 1))
+            tmp[0:img.shape[0], 0:w] = img
+            img = tmp
+        return img
+
+    def _predict_from_array(self, data: Tuple[np.ndarray]) -> Tuple[np.ndarray]:
+        predicted = []
+        for idx in range(len(data)):
+            height, width = data[idx].shape[:2]
+            img = self._add_outline(data[idx])
+            new_height, new_width = img.shape[:2]
+            patches = self._get_patches(img)
+            predictions = self.model.predict(patches, batch_size=self.batch_size, verbose=1)
+            pred_img = self._build_img_from_patches(predictions, new_height, new_width)
+
+            pred_img = pred_img[:height, :width]
+            predicted.append(pred_img)
+        return predicted
+
+    def _predict_from_path(self, path: Union[str, Tuple[str]]) -> Tuple[np.ndarray]:
+        images = []
+
+        if isinstance(path, (list, tuple)):
+            for im_path in path:
+                images.append(io.imread(im_path, as_gray=True))
+        if os.path.isfile(path):
+            images.append(io.imread(path, as_gray=True))
+        if os.path.isdir(path):
+            for im_path in glob(os.path.join(path, '*')):
+                images.append(io.imread(im_path, as_gray=True))
+        for i in range(len(images)):
+            if len(images[i].shape) != 3:
+                images[i] = images[i][..., np.newaxis]
+                if images[i].max() > 1.0:
+                    images[i] = images[i] / 255.0
+        return self._predict_from_array(images)
+
+    def predict(self, data: Union[np.ndarray, Tuple[np.ndarray], str]) -> Tuple[np.ndarray]:
+        if isinstance(data, str):
+            return self._predict_from_path(data)
+        if isinstance(data, np.ndarray):
+            return self._predict_from_array([data])
+        if isinstance(data, (list, tuple)) and len(data) > 0:
+            if isinstance(data[0], str):
+                return self._predict_from_path(data) 
+            if isinstance(data[0], np.ndarray):
+                return self._predict_from_array(data)
+            
 
 if __name__ == '__main__':
-    model: Model = load_model(r'generator\models\20220325-1620\model_last.h5')
-    if 0:
-        data = HexagonDataIterator(1, 64, 5, noise_size=(64, 64, 1))
-        for x, z in data:
-            # plt.imshow(x[0], cmap='gray')
-            # plt.show()
-            # x = morphology.dilation(x[0, ..., 0], morphology.square(2))[np.newaxis, ..., np.newaxis]
-            xsap = add_salt_and_pepper(x, 0.1)
-            psap = ((model.predict_on_batch([xsap, z])[0] + 1) / 2)
-            p = ((model.predict_on_batch([x, z])[0] + 1) / 2)
-            print(p.shape)
-            x = x[0]
-            xsap = xsap[0]
-            xy = np.concatenate([x, p, psap, xsap], axis=1)
-            plt.imshow(xy, cmap='gray')
-            plt.show()
-    elif 1:
-        train, test = load_dataset(r'datasets\Alizarine\folds.json')[0]
-        data = DataIterator(test, batch_size=1, patch_per_image=10)
-        for x, y in data:
-            z = np.random.normal(size=x.shape)
-            xsap = add_salt_and_pepper(
-                x, sap_ratio=0.05, salt_value=0.8, keep_edges=False)
-            p = model.predict_on_batch([xsap, z])[0]
-            x = x[0]
-            y = y[0]
-            xsap = xsap[0]
-            p = normalization(p)
-            y = normalization(y)
-            plt.hist(p.flatten(), bins=100, range=(0, 1))
-            plt.show()
-            plt.hist(y.flatten(), bins=100, range=(0, 1))
-            plt.show()
-            plt.imshow(np.concatenate([xsap, x, p, y], axis=1), cmap='gray')
-            plt.show()
+    unet_pred = UnetPrediction(r'segmentation\models\20220401-0004\model.hdf5',  stride = 16, batch_size = 128)
+    pred = unet_pred.predict(r'datasets\Alizarine\images\5.png')
+    for im in pred:
+        plt.imshow(im, 'gray')
+        plt.show()
+    
--- train_segmentation.py
+++ train_segmentation.py
@@ -16,20 +16,21 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 # os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 if __name__ == '__main__':
-    synthetic_dataset = generate_dataset(r'generator\models\20220330-1516\model_last.h5', num_of_data=128 * 1, batch_size=128)
+    synthetic_dataset = generate_dataset(r'generator\models\20220330-1516\model_last.h5', num_of_data=128 * 100, batch_size=256)
     image, mask = synthetic_dataset
     for i in range(len(image)):
-        rgamma = np.random.uniform(0.5, 1.5)
+        rgamma = np.random.uniform(0.25, 1.75)
         image[i] = exposure.adjust_gamma(image[i], rgamma)
         add_marker_to_mask(mask[i])
     synthetic_dataset = (image, mask)
     print(image.min(), image.max(), mask.min(), mask.max())
 
     
-    # unet = SegmentationUnet()
-    # unet.summary()
+    unet = SegmentationUnet()
+    unet.summary()
 
-    # fold = 0
-    # train, test  = load_dataset(r'datasets\Alizarine\folds.json', normalize=False)[fold]
-    # validation_data = DataIterator(test, 1, patch_per_image=1, inv_values=False).get_dataset()
-    # unet.train(50, synthetic_dataset, validation_data, validation_split=0.1)
+    fold = 0
+    train, test  = load_dataset(r'datasets\Alizarine\folds.json', normalize=False)[fold]
+    # image, mask = DataIterator(train, 1, inv_values=False).get_dataset()
+    validation_data = DataIterator(test, 1, patch_per_image=1, inv_values=False).get_dataset()
+    unet.train(50, (image, mask), validation_data, validation_split=0.1)
